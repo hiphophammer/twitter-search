19c996d15a909c86cd69411e28ba67b3bca8944a
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-02 01:22:18 -0700

    Renamed navBar to topMenu

---
+++ src/components/topMenu.tsx
@@ -0,0 +1,55 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import KRFlag from './flags/kor.svg';
+import USFlag from './flags/usa.svg';
+
+const TopMenu = ( ) => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">트위터 검색기</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="https://github.com/hiphophammer" target="_blank" rel="noopener noreferrer">GitHub</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#KR">
+              <img 
+                alt='Korean'
+                src={KRFlag}
+                height='20'
+                className="d-inline-block align-center"
+              />{' '}
+            </Nav.Link>
+            <Nav.Link href="#EN_US">
+              <img 
+                alt='English (United States)'
+                src={USFlag}
+                height='20'
+                className="d-inline-block align-center"
+              />{' '}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopMenu;
