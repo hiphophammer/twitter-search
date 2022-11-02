9368e4302e2e02661cec241d6ea2fb101f1bdbe2
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-02 01:22:50 -0700

    Menu component for footer

---
+++ src/components/bottomMenu.tsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Alert from 'react-bootstrap/Alert';
+
+const BottomMenu = ( ) => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="bottom">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link href="https://github.com/hiphophammer" target="_blank" rel="noopener noreferrer">GitHub</Nav.Link>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BottomMenu;
