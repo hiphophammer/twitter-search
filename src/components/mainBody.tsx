12def7ed23b1c400255928735c0b25d0297e0233
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-02 08:24:08 -0700

    Update mainBody.tsx

--- src/components/mainBody.tsx
+++ src/components/mainBody.tsx
@@ -1,9 +1,12 @@
 import Container from 'react-bootstrap/Container';
+import ConfirmButton from './confirmButton'
+import AdvancedSearchSwitch from './advancedSearchSwitch';
 
 const MainBody = ( ) => {
   return (
     <Container>
-      ㅇㅅㅇ;
+      <AdvancedSearchSwitch />
+      <ConfirmButton />
     </Container>
   );
 }
