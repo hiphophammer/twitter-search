9be2bc6bd23f586254a89f239d8aed627686acd0
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-03 03:02:44 -0700

    switchStatus cannot be undefined anymore

--- src/components/interfaces.tsx
+++ src/components/interfaces.tsx
@@ -1,13 +1,18 @@
 interface ToggleSwitchProp {
   label: string,
-  switchStatus: boolean | undefined,
-  setSwitchStatus: React.Dispatch<React.SetStateAction<boolean | undefined>>
+  switchStatus: boolean,
+  setSwitchStatus: React.Dispatch<React.SetStateAction<boolean>>,
+  setHeight?: {
+    cardHeight: number
+    setCardHeight:React.Dispatch<React.SetStateAction<number>>, 
+    heightOffset:number
+  } // optional height adjust hook
 }
 
 interface TextBoxProp {
   controlID: string,
   floatingLabel: string,
-  textBoxRef: React.RefObject<HTMLInputElement>
+  textBoxRef: React.RefObject<HTMLInputElement>,
 }
 
 export { type ToggleSwitchProp, type TextBoxProp };
