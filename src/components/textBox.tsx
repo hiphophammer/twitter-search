2d9d22e06b388952bf0cf8fb9ffcaae7d4e464ff
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-03 01:11:06 -0700

    created textbox component with floating label

---
+++ src/components/textBox.tsx
@@ -0,0 +1,16 @@
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+import { TextBoxProp } from './interfaces';
+
+const TextBox = ( props:TextBoxProp ) => {
+  return (
+    <>
+      <FloatingLabel controlId={ props.controlID } label={ props.floatingLabel }>
+        <Form.Control placeholder={ props.floatingLabel } ref={ props.textBoxRef } />
+      </FloatingLabel>
+    </>
+  );
+}
+
+export default TextBox;
