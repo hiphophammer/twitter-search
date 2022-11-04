4ecbb417822e14607f65e924baf7e16763c348d5
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-04 06:34:55 -0700

    Create settingsModal.tsx

---
+++ src/components/settingsModal.tsx
@@ -0,0 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { ShowModal } from './interfaces';
+
+function SettingsModal( props:ShowModal ) {
+  const handleClose = () => props.setShow(false);
+
+  return (
+    <>
+      <Modal show={props.show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>업데이트 내역</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          ㅈㅅ 아직 아무것도 되는 게 없음 ㅎ;; ㅠ <br />
+          Sry.. nothing works as of yet.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            닫기
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default SettingsModal;
