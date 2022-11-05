4f87bee7fa0d6f831865e44c605e8d176fbf1c13
Jeongwook Oh <hiphophammer@hotmail.com>
2022-11-05 08:55:56 -0700

    removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import TopMenu from './components/topMenu';
 import MainBody from './components/mainBody';
 import BottomMenu from './components/bottomMenu';
-
-import { LangProp } from './components/interfaces';
 import { useState } from 'react';
-import './App.module.css';
 
 function App() {
   const [ langObj, setLangObj ] = useState( require( './components/locale/ko_kr.json' ) );
